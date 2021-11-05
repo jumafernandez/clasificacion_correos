a887e21430cfdca0f874794167acf39b6f603f5c
Juan Manuel Fernandez <juanmanuelunlu@gmail.com>
2021-11-05 12:06:19 -0300

    Se incorporan los últimos scripts a la Tesis.

---
+++ tesis/scripts/supervisado/funciones_clasificacion_texto.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 25 11:24:29 2021
+
+@author: unlu
+"""
+
+def representacion_documentos(textos_train, textos_test, estrategia, MAX_TKS):
+  ''' esta función recibe las consultas de train y test y genera las 
+  features dinámicas en base a 
+  5 estrategias = {BASELINE, BOW, TFIDF, 3-4-NGRAM-CHAR, 1-2-NGRAM-WORD} 
+  '''
+  # Vamos a probar 4 estrategias de representación de documentos
+  from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+  import pandas as pd
+  
+  # Inicializamos el vectorizer de acuerdo a la estrategia de representación
+  if(estrategia=="BASELINE"):
+    vectorizer = CountVectorizer(max_features=MAX_TKS)
+
+  # Inicializamos el vectorizer de acuerdo a la estrategia de representación
+  if(estrategia=="BOW"):
+    vectorizer = CountVectorizer(token_pattern = '[\w\/\%]+', max_features=MAX_TKS)
+    
+  if(estrategia=="TFIDF"):
+    vectorizer = TfidfVectorizer(token_pattern = '[\w\/\%]+', max_features=MAX_TKS)
+             
+  if(estrategia=="3-4-NGRAM-CHARS"):
+    vectorizer = TfidfVectorizer(analyzer='char', ngram_range=(3,4), token_pattern = '[\w\/\%]+', max_features=MAX_TKS)
+    
+  if(estrategia=="1-2-NGRAM-WORDS"):
+    vectorizer = TfidfVectorizer(analyzer='word', ngram_range=(1,2), token_pattern = '[\w\/\%]+', max_features=MAX_TKS)
+
+  # Entrenamos el vectorizer para train y test
+  representacion_correos_train = vectorizer.fit_transform(textos_train)
+  representacion_correos_test = vectorizer.transform(textos_test)     
+
+  # Convertimos las matrices ralas a series para concaterlas con los df originales luego
+  df_representacion_correos_train = pd.DataFrame.sparse.from_spmatrix(representacion_correos_train, columns=vectorizer.get_feature_names())
+  df_representacion_correos_test = pd.DataFrame.sparse.from_spmatrix(representacion_correos_test, columns=vectorizer.get_feature_names())
+  
+  return df_representacion_correos_train, df_representacion_correos_test
+
+
+def gridsearch_por_estrategia_representacion(train, test, estrategia, tecnica, parameters, results_save):
+  from funciones_dataset import consolidar_df
+  from sklearn.pipeline import Pipeline
+  from sklearn.model_selection import GridSearchCV
+  from sklearn.preprocessing import MinMaxScaler
+  from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, matthews_corrcoef
+  import pandas as pd
+
+  # Esta función va dentro de un iterador entre las 5 estrategias    
+  print('Estrategia de representación: {}' . format(estrategia))
+  correos_train_vec, correos_test_vec = representacion_documentos(train['Consulta'], test['Consulta'], estrategia, None)
+
+  # Separo en x e y - train y test- (además consolido feature estáticas con dinámicas)
+  x_train, y_train = consolidar_df(train, correos_train_vec, 'Consulta', 'clase')
+  x_test, y_test = consolidar_df(test, correos_test_vec, 'Consulta', 'clase')
+  
+  # Escalado de datos: Se probó scale y MinMaxScaler y dió mejores resultados el último
+  scaler = MinMaxScaler()
+  x_train_scaled = scaler.fit_transform(x_train)
+  x_test_scaled = scaler.fit_transform(x_test)
+
+  # Dejo Pipeline, si bien no tiene sentido, queda por retrocompatibilidad con versión anterior
+  if tecnica == 'SVM':
+    from sklearn.svm import SVC
+    model_pipe = Pipeline([(tecnica, SVC())])
+  else:
+    from xgboost import XGBClassifier
+    model_pipe = Pipeline([(tecnica, XGBClassifier())])
+
+    
+  # Instancio y "entreno" el GridSearchCV
+  grid_search=GridSearchCV(model_pipe, param_grid=parameters, cv=None, n_jobs=-1, verbose=3)
+  grid_search.fit(x_train_scaled, y_train)
+
+  # Se realizan las predicciones sobre el conjunto de validación
+  grid_predictions = grid_search.predict(x_test_scaled) 
+
+  # Paso los resultados y el accuracy a un dataframe
+  results_train = pd.concat([pd.DataFrame(grid_search.cv_results_["params"]), pd.DataFrame(grid_search.cv_results_["mean_test_score"], columns=["Accuracy"])],axis=1)
+  results_train['estrategia'] = estrategia
+  
+  # Los guardo
+  results_train.to_csv('results_train.csv', mode='a')
+ 
+  # Calculo las métricas sobre test para el paper
+  acc_test = accuracy_score(y_test, grid_predictions)
+  precision_test = precision_score(y_test, grid_predictions, average='weighted')
+  recall_test = recall_score(y_test, grid_predictions, average='weighted')
+  f1_test = f1_score(y_test, grid_predictions, average='weighted')
+  mcc = matthews_corrcoef(y_test, grid_predictions)
+ 
+  # Genero un diccionario con la métricas obtenidas en test
+  # dict_grid_test = grid_search.best_params_
+  dict_grid_test = {}
+  dict_grid_test['clasificador'] = tecnica
+  dict_grid_test['estrategia'] = estrategia
+  dict_grid_test['accuracy'] = acc_test
+  dict_grid_test['precision'] = precision_test
+  dict_grid_test['recall'] = recall_test
+  dict_grid_test['f1_score'] = f1_test
+  dict_grid_test['mcc'] = mcc
+ 
+  if (results_save=='drive'):
+ 
+    # Paso el diccionario a dataframe  
+    results_test = pd.DataFrame([dict_grid_test])
+
+    results_test.to_csv('results_test.csv', mode='a')
+
+    # Autenticación y guardado en Drive
+    import os
+    from google.colab import drive
+    drive.mount('drive')
+    os.system('results_test.csv drive/My Drive/')
+
+  
+  print('Estrategia de representación: {}' . format(estrategia))
+  print('Parámetros: {}' . format(grid_search.best_params_))
+  print('Accuracy Test-Set: {}' . format(acc_test))
+  print('Métricas sobre Test-Set: {}' . format(dict_grid_test))
+
---
+++ tesis/scripts/supervisado/funciones_dataset.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 24 22:46:55 2021
+
+@author: unlu
+"""
+
+def get_clases():
+  '''
+  Esta función retorna las etiquetas de las clases sobre el total de los correos. 
+  Tomado de notebooks/jcc/de train_test_data.ipynb
+  '''
+  import numpy as np
+
+  etiquetas_clases = np.array(['Boleto Universitario', 
+                               'Cambio de Carrera', 
+                               'Cambio de Comisión',
+                               'Carga de Notas', 
+                               'Certificados Web', 
+                               'Consulta por Equivalencias',
+                               'Consulta por Legajo', 
+                               'Consulta sobre Título Universitario',
+                               'Cursadas', 
+                               'Datos Personales', 
+                               'Exámenes',
+                               'Ingreso a la Universidad',
+                               'Inscripción a Cursadas',
+                               'Pedido de Certificados',
+                               'Problemas con la Clave',
+                               'Reincorporación', 
+                               'Requisitos de Ingreso',
+                               'Simultaneidad de Carreras', 
+                               'Situación Académica',
+                               'Vacunas Enfermería'])
+
+  return etiquetas_clases
+
+def cargar_dataset(URL_data, file_train, file_test, descarga, nombre_clase, class_labels, cantidad_clases, texto_otras):
+  '''
+  Carga los train y test set y genera la reducción de clases, en caso que sea necesario
+  '''
+  import pandas as pd
+  import numpy as np
+  import warnings
+  warnings.filterwarnings("ignore")
+
+  # Genero el enlace completo
+  URL_file_train = URL_data + file_train
+  URL_file_test = URL_data + file_test
+  
+  if descarga:
+    print('Se inicia descarga de los datasets.'.format(file_train, file_test))
+    import wget
+    wget.download(URL_file_train)
+    wget.download(URL_file_test)
+  else:
+    file_train = URL_file_train
+    file_test = URL_file_test
+    
+  # Leemos el archivo en un dataframe
+  df_train = pd.read_csv(file_train)
+  df_test = pd.read_csv(file_test)
+
+  # Agrupamiento de clases
+  etiquetas_clases = class_labels
+  if cantidad_clases<len(class_labels):
+    # Se realiza un conteo de frecuencia por clase y se toman los correos que pertenecen a 
+    # las N-cantidad_clases menos observadas
+    clases = df_train.clase.value_counts()
+    clases_minoritarias = clases.iloc[cantidad_clases-1:].keys().to_list()
+
+    # Agrego a las etiquetas la etiqueta "Otras Consultas" para el agrupamiento
+    etiquetas_clases = np.append(class_labels, texto_otras)
+
+    # Genero una nueva clave de clases para "Otras Consultas" a modo de agrupar las que poseen menos apariciones
+    df_train.clase[df_train[nombre_clase].isin(clases_minoritarias)] = np.where(etiquetas_clases == texto_otras)[0]
+    df_test.clase[df_test[nombre_clase].isin(clases_minoritarias)] = np.where(etiquetas_clases == texto_otras)[0]
+
+  print("\nEl conjunto de entrenamiento tiene la dimensión: " + str(df_train.shape))
+  print("El conjunto de testeo tiene la dimensión: " + str(df_test.shape))
+
+  return df_train, df_test, etiquetas_clases
+
+
+def consolidar_df(df, features_dinamicas_vec, atributo_consulta, atributo_clase):
+  '''
+  Función para unir features dinámicas a las estáticas y separar en x e y
+  '''
+  import pandas as pd
+  
+  # Separo en x e y -train-
+  y = df[atributo_clase].to_numpy()
+  x = pd.concat([df.drop([atributo_consulta, atributo_clase], axis=1), features_dinamicas_vec], axis=1)
+  
+  return x, y
+
+  
+def add_clase(array, clase):
+  '''
+  Creo esta función para agregar la etiqueta Otras consultas
+  '''
+  import numpy as np
+  array_nuevo = np.append(array, clase)
+  return array_nuevo
+
+def separar_x_y_rna(df, atributo_consulta, atributo_clase):
+  '''
+  Función para separar en x e y
+  '''
+  # Separo en x e y
+  X = df[atributo_consulta]
+  y = df[atributo_clase].to_numpy()
+
+  return X, y  
